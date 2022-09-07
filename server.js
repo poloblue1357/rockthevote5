944cecdf66d577a6a18d0de7b70f0dd0373452ef
Dan Patterson <starved14@hotmail.com>
2022-09-07 12:31:10 -0600

    testing change

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
+// please work!!!!
+
 // mongoose.connect(process.env.MONGODB_URI,
 //     {
 //         // useNewUrlParser: true,
